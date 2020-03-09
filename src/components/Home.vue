fd302842487ee96859ddacaf75e15a3aa1f14a64
unknown <yokoyamayuj@gmail.com>
2020-03-09 15:05:03 +0900

    initial

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="newpage">
+    <h1>ようこそ {{name}} さん</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      require: true
+    }
+  }
+}
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="login">
+    <form @submit.prevent>
+        <input v-model='name' placeholder='user name'/><br/>
+        <input v-model='pass' placeholder='password'/><br/>
+        <button type="submit" @click='handleSubmit'>ログイン</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      name: '',
+      pass: ''
+    }
+  },
+  methods: {
+    handleSubmit: function () {
+      console.log(this.name)
+      // 本来はajax通信をする
+      this.$router.push({ name: 'Home', params: { name: this.name } })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
